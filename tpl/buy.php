627541e2af722caa66a0a07a0ee96799de2f9f8a
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-18 18:38:02 +0900

    [update] .html を .php に変更

---
+++ tpl/buy.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./../css/destyle.css">
+  <link rel="stylesheet" href="./../css/style.css">
+  <title>購入画面</title>
+</head>
+
+<body>
+  <main>
+    <article>
+      <h2></h2>
+      <section class="img_product">
+        <img src="./../img/アシメレースアップリボンニット.png" alt="商品画像" width="315" height="420">
+      </section>
+      <section>
+        <h2>商品説明</h2>
+        <ul>
+          <li>アシメレースアップリボンニット（長袖）</li>
+          <li>LAISSE PASSE</li>
+          <li>¥3,600</li>
+        </ul>
+        <table>
+          <tr>
+            <th>カテゴリ</th>
+            <td>ブラウス</td>
+          </tr>
+          <tr>
+            <th>商品の状態</th>
+            <td>目立った傷や汚れなし</td>
+          </tr>
+          <tr>
+            <th>発送までの日数</th>
+            <td>2~3日</td>
+          </tr>
+          <tr>
+            <th>商品説明</th>
+            <td>2020年頃に1万5000円で購入しましたニットです。エクリュカラーで傷はありません。合わせやすいのでおすすめです。</td>
+          </tr>
+        </table>
+        <p>商品を閉じる</p>
+      </section>
+
+      <section>
+        <h2>出品者</h2>
+        <p><img src="./../img/Profile_Mari.png" alt="プロフィール画像"></p>
+        <p>Mari さん</p>
+        <p>メッセージ</p>
+      </section>
+      <section>
+        <h2>配送先</h2>
+        <form action="" method="post">
+          <ul>
+            <li>
+              <input type="radio" name="" id="">配送先1 <br>
+              〒530-0001 <br>
+              大阪市北区梅田3-3-1
+            </li>
+            <li>
+              <input type="radio" name="" id="">配送先2 <br>
+              〒160-0023 <br>
+              東京新宿区西新宿1-7-3
+            </li>
+            <li><label for=""><span>+</span>新しい配送先を追加する</label></li>
+          </ul>
+
+          <h2>オプション</h2>
+          <input type="checkbox" name="" id="">クリーニングあり
+          <a href="">取引中止を申請する</a>
+          <a href="">問題を報告する</a>
+
+          <button type="submit" name="buy_btn" value="on">購入する</button>
+
+        </form>
+      </section>
+
+    </article>
+  </main>
+</body>
+
+</html>
