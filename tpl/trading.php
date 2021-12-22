2376d4e4da43bb026668ade08a18f016fbbe21e5
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-22 15:49:25 +0900

    [update]画像のパスを変更

--- tpl/trading.php
+++ tpl/trading.php
@@ -1,72 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./../css/trading.css" />
-    <title>取引</title>
-  </head>
-  <body>
-    <main>
-      <article>
-        <section class="sec_product">
-          <div>
-            <img
-              src="./../img/products/product1.jpg"
-              alt="商品画像サムネイル"
-              width="80px"
-              height="80px"
-            />
-          </div>
-          <div>
-            <p>
-              アシメレースリボンニット（長袖）<br />
-              ¥3,600
-            </p>
-          </div>
-        </section>
 
-        <section>
-          <h2>配送状況</h2>
-          <ul>
-            <li>
-              <img
-                src="./../img/progress.png"
-                alt="配送状況プログレスバー"
-                width="375px"
-                height=""
-              />
-            </li>
-            <li>発送準備中</li>
-            <li>発送済</li>
-            <li>配達中</li>
-            <li>配達済</li>
-          </ul>
-          <p class="center">配達済</p>
-        </section>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/trading.css" />
+  <title>取引</title>
+</head>
 
-        <section>
-          <h2>出品者</h2>
+<body>
+  <main>
+    <article>
+      <section class="sec_product">
+        <div>
+          <img src="./img/users/2/products/1/product1.jpg" alt="商品画像サムネイル" width="80px" height="80px" />
+        </div>
+        <div>
           <p>
-            <img
-              src="./../img/users/Profile_Mari.png"
-              alt="プロフィール画像"
-              width="32px"
-              height="32px"
-            />
-            Mari さん
+            アシメレースリボンニット（長袖）<br />
+            ¥3,600
           </p>
-          <p><button type="submit" class="btn btn_msg">メッセージ</button></p>
-        </section>
-        <div class="center">
-          <a href="">返品を申請する</a><br />
-          <a href="">問題を報告する</a>
         </div>
-        <div class="bg_btn_eval">
-          <button type="submit" class="btn btn_eval">評価画面へ</button>
-        </div>
-      </article>
-    </main>
-  </body>
+      </section>
+
+      <section>
+        <h2>配送状況</h2>
+        <ul>
+          <li>
+            <img src="./img/progress.png" alt="配送状況プログレスバー" width="375px" height="" />
+          </li>
+          <li>発送準備中</li>
+          <li>発送済</li>
+          <li>配達中</li>
+          <li>配達済</li>
+        </ul>
+        <p class="center">配達済</p>
+      </section>
+
+      <section>
+        <h2>出品者</h2>
+        <p>
+          <img src="./img/users/2/prof/mari.jpg" alt="プロフィール画像" width="32px" height="32px" />
+          Mari さん
+        </p>
+        <p><button type="submit" class="btn btn_msg">メッセージ</button></p>
+      </section>
+      <div class="center">
+        <a href="">返品を申請する</a><br />
+        <a href="">問題を報告する</a>
+      </div>
+      <div class="bg_btn_eval">
+        <button type="submit" class="btn btn_eval">評価画面へ</button>
+      </div>
+    </article>
+  </main>
+</body>
+
 </html>
