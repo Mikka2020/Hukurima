7c436befbc139fce56088a983d8881b682ccf601
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-25 01:15:06 +0900

    [add]新規作成

---
+++ tpl/footer.php
@@ -0,0 +1,29 @@
+<nav>
+  <ul>
+    <li>
+      <a href="./index.php">
+        <img src="./../img/icon/index.png" alt="homeアイコン" width="24px" height="24px">ホーム
+      </a>
+    </li>
+    <li>
+      <a href="./search.php">
+        <img src="./../img/icon/search.png" alt="searchアイコン" width="24px" height="24px">探す
+      </a>
+    </li>
+    <li>
+      <a href="./listing.php">
+        <img src="./../img/icon/listing.png" alt="listingアイコン" width="24px" height="24px">出品
+      </a>
+    </li>
+    <li>
+      <a href="./notification.php">
+        <img src="./../img/icon/notification.png" alt="notificationアイコン" width="24px" height="24px">通知
+      </a>
+    </li>
+    <li>
+      <a href="./mypage.php">
+        <img src="./../img/icon/mypage.png" alt="mypageアイコン" width="24px" height="24px">マイページ
+      </a>
+    </li>
+  </ul>
+</nav>
