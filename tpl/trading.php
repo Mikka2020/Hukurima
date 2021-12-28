f5419757bfc3d854f32b92037e35325379391846
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-29 02:18:17 +0900

    [update]プログレスバーをcssで表示

--- tpl/trading.php
+++ tpl/trading.php
@@ -12,9 +12,10 @@
 <body>
   <main>
     <article>
-      <section class="sec_product">
+      <section class="product-area">
         <div>
-          <img src="./img/users/2/products/1/product1.jpg" alt="商品画像サムネイル" width="80px" height="80px" />
+          <img src="./img/users/<?php echo "2"; ?>/products/<?php echo "1"; ?>/<?php echo 'アシメレースアップリボンニット'; ?>_1.jpg"
+            alt="商品画像サムネイル" width="80px" height="80px" />
         </div>
         <div>
           <p>
@@ -26,15 +27,21 @@
 
       <section>
         <h2>配送状況</h2>
-        <ul>
-          <li>
-            <img src="./img/progress.png" alt="配送状況プログレスバー" width="375px" height="" />
-          </li>
-          <li>発送準備中</li>
-          <li>発送済</li>
-          <li>配達中</li>
-          <li>配達済</li>
-        </ul>
+        <!-- <img src="./img/progress.png" alt="配送状況プログレスバー" width="375px" height="" /> -->
+        <div class="progress-bar-area">
+          <ul class="progress-bar">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <ul class="send-status">
+            <li>発送準備中</li>
+            <li>発送済</li>
+            <li>配達中</li>
+            <li>配達済</li>
+          </ul>
+        </div>
         <p class="center">配達済</p>
       </section>
 
@@ -44,14 +51,15 @@
           <img src="./img/users/2/prof/mari.jpg" alt="プロフィール画像" width="32px" height="32px" />
           Mari さん
         </p>
-        <p><button type="submit" class="btn btn_msg">メッセージ</button></p>
+        <p><button type="submit" class="btn msg-btn">メッセージ</button></p>
       </section>
+
       <div class="center">
         <a href="">返品を申請する</a><br />
         <a href="">問題を報告する</a>
       </div>
       <div class="bg_btn_eval">
-        <button type="submit" class="btn btn_eval">評価画面へ</button>
+        <button type="submit" class="btn eval-btn">評価画面へ</button>
       </div>
     </article>
   </main>
