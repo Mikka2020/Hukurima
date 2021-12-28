ebfb17bb0743ecf8300a0d0332a6082492a8e3ae
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-28 23:28:47 +0900

    [add]テスト用の関数を作成、後で消す

--- product_detail.php
+++ product_detail.php
@@ -1,3 +1,15 @@
 <?php
 
+// テスト用 高橋
+require_once './config.php';
+require_once './func.php';
+$table = "listing";
+$conditions = ['search' => '', 'sort' => 'favorite', 'trend' => ''];
+$link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
+mysqli_set_charset($link, 'utf8');
+$line = get_column_order($link, $table, $conditions);
+mysqli_close($link);
+$product = $line[0];
+// var_dump($product);
+
 require_once('./tpl/product_detail.php');
