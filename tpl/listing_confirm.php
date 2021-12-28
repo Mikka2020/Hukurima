4abc9ea091683d855965fd9e2b656f47da8b077f
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-29 00:48:36 +0900

    [update]処理側で取得したデータを表示できるように変更

--- tpl/listing_confirm.php
+++ tpl/listing_confirm.php
@@ -5,36 +5,80 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/listing_confirm.css">
   <title>出品確認</title>
 </head>
 
 <body>
+  <header></header>
   <main>
+    <h1>フクリマ</h1>
     <article>
-      <img src="" alt="メイン商品画像">
-      <img src="" alt="その他の商品画像">
-      <img src="" alt="その他の商品画像">
-      <img src="" alt="その他の商品画像">
-      <img src="" alt="その他の商品画像">
-      <img src="" alt="その他の商品画像">
-      <img src="" alt="その他の商品画像">
-      <section>
-        <p><?php ?></p> <!-- 商品名 -->
-        <p>カテゴリ</p>
-        <p><?php  ?></p> <!-- カテゴリ -->
-        <p>商品の状態</p>
-        <p><?php ?></p> <!-- 商品の状態 -->
-        <p>発送までの日時</p>
-        <p><?php ?></p> <!-- 発送までの日時 -->
-        <p>商品説明</p>
-        <p><?php ?></p> <!-- 商品説明 -->
-        <div>
-          <a href="./search.php?val=<?php  ?>"></a> <!-- タグをforeachで複数表示 -->
-        </div>
+      <h2><?php echo ''; // 商品名 ?></h2>
+      <img
+        src="./img/users/<?php echo 2; // 会員id ?>/products/<?php echo 1; // 商品id ?>/<?php echo 'アシメレースアップリボンニット'; // 商品名 ?>_1.jpg"
+        alt="メイン商品画像" width="360px" height="">
+      <ul>
+        <?php // foreach($images as $image): ?>
+        <li>
+          <img
+            src="./img/users/<?php echo 2; // 会員id ?>/products/<?php echo 1; // 商品id ?>/<?php echo 'アシメレースアップリボンニット'; // 商品名 ?>_1.jpg"
+            alt="その他の商品画像" width="32px" height="32px">
+        </li>
+        <?php // endforeach; ?>
+      </ul>
+
+      <section class="product-detail">
+        <h3>商品説明</h3>
+        <p><?php echo 'アシメレースアップリボンニット'; // 商品名 ?></p>
+        <ul>
+          <li>
+            カテゴリ
+          </li>
+          <li>
+            <?php echo 'ブラウス'; ?>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            商品の状態
+          </li>
+          <li>
+            <?php echo '目立った傷や汚れ無し'; ?>
+          </li>
+        </ul>
+        <ul>
+          <li>発送までの日時</li>
+          <li><?php echo '1~3日' ?></li>
+        </ul>
+        <ul>
+          <li>商品説明</li>
+          <li><?php echo '2020年頃に1万5000円で購入しましたニットです。エクリュカラーで傷はありません。'; ?></li>
+        </ul>
+
+      </section>
+
+      <section class="tag-area">
+        <h3>タグ</h3>
+        <ul>
+          <!-- タグをforeachで複数表示 -->
+          <?php // foreach($tags as $tag): ?>
+          <li>
+            <?php echo 'ニット'; ?>
+          </li>
+          <li>
+            <?php echo 'ブラウス'; ?>
+          </li>
+          <?php // endforeach; ?>
+        </ul>
       </section>
     </article>
-    <button type="submit">出品する</button>
+    <form action="" method="post">
+      <button type="submit" class="btn">出品する</button>
+      <button type="submit" class="btn">修正する</button>
+    </form>
   </main>
+  <footer></footer>
 </body>
 
 </html>
