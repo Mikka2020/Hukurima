2ed1d2554ca2bd56430138e28afdff55d5189d99
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-28 19:59:28 +0900

    [fix]相対パスを修正

--- tpl/footer.php
+++ tpl/footer.php
@@ -2,27 +2,27 @@
   <ul>
     <li>
       <a href="./index.php">
-        <img src="./../img/icon/index.png" alt="homeアイコン" width="24px" height="24px">ホーム
+        <img src="./img/icon/index.png" alt="homeアイコン" width="24px" height="24px"><br>ホーム
       </a>
     </li>
     <li>
       <a href="./search.php">
-        <img src="./../img/icon/search.png" alt="searchアイコン" width="24px" height="24px">探す
+        <img src="./img/icon/search.png" alt="searchアイコン" width="24px" height="24px"><br>探す
       </a>
     </li>
     <li>
       <a href="./listing.php">
-        <img src="./../img/icon/listing.png" alt="listingアイコン" width="24px" height="24px">出品
+        <img src="./img/icon/listing.png" alt="listingアイコン" width="24px" height="24px"><br>出品
       </a>
     </li>
     <li>
       <a href="./notification.php">
-        <img src="./../img/icon/notification.png" alt="notificationアイコン" width="24px" height="24px">通知
+        <img src="./img/icon/notification.png" alt="notificationアイコン" width="24px" height="24px"><br>通知
       </a>
     </li>
     <li>
       <a href="./mypage.php">
-        <img src="./../img/icon/mypage.png" alt="mypageアイコン" width="24px" height="24px">マイページ
+        <img src="./img/icon/mypage.png" alt="mypageアイコン" width="24px" height="24px"><br>マイページ
       </a>
     </li>
   </ul>
