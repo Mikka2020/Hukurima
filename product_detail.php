7fd4665b0b27d7147709cc10444823eb235108ae
ash <ogawa.sato01@gmail.com>
2021-12-29 15:04:19 +0900

    [update] 新規作成　高橋

--- product_detail.php
+++ product_detail.php
@@ -3,13 +3,18 @@
 // テスト用 高橋
 require_once './config.php';
 require_once './func.php';
+
+// 初期設定
 $table = "listing";
 $conditions = ['search' => '', 'sort' => 'favorite', 'trend' => ''];
+
+// データ取得処理
 $link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
 mysqli_set_charset($link, 'utf8');
 $line = get_column_order($link, $table, $conditions);
 mysqli_close($link);
 $product = $line[0];
-// var_dump($product);
+
+var_dump($product);
 
 require_once('./tpl/product_detail.php');
