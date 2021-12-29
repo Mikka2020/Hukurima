f47567913b38ffcf34a72d452bc7adcb16e3c441
mikka2020 <takahashi.yuji.as01@gmail.com>
2021-12-29 20:42:31 +0900

    [update]DBから取得した商品情報を表示できるように変更

--- tpl/_listed_history.php
+++ tpl/_listed_history.php
@@ -35,12 +35,12 @@
             <ul>
               <li>
                 <button class="product_btn">
-                  <img src="./img/icon/いいねアイコン.png" alt="いいねアイコン" class="favorite_icon">3
+                  <img src="./img/icon/いいねアイコン.png" alt="いいねアイコン" class="favorite_icon"><?php echo "3"; ?>
                 </button>
               </li>
               <li>
                 <button class="product_btn">
-                  <img src="./img/icon/コメントアイコン.png" alt="コメントアイコン" class="comment_icon">1
+                  <img src="./img/icon/コメントアイコン.png" alt="コメントアイコン" class="comment_icon"><?php echo "1"; ?>
                 </button>
               </li>
             </ul>
@@ -53,56 +53,54 @@
         <div>
           <p class="product_text"><?php echo "2020年頃に1万5000円で購入しましたニットです。エクリュカラーで傷はありません。合わせやすいのでおすすめです。"; // 商品詳細 ?>
           </p>
-          <p><img src="./img/icon/clock-regular.svg" alt="時計アイコン" width="12px" height="">12時間前</p>
+          <p><img src="./img/icon/clock-regular.svg" alt="時計アイコン" width="12px" height=""><?php echo "12時間前"; ?></p>
         </div>
         <div class="tag_area">
           <ul class="tag_ul">
-            <li class="tag">#ニット</li>
-            <li class="tag">#ブラウス</li>
-            <li class="tag">#リボン</li>
-            <li class="tag">#レース</li>
-            <li class="tag">#長袖</li>
-            <li class="tag">#女性</li>
+            <?php // foreach($tags as $tag): ?>
+            <li class="tag"><?php echo "#ニット"; ?></li>
+            <?php // endforeach; ?>
           </ul>
         </div>
       </section>
 
       <h4>商品詳細</h4>
       <section class="product_info_area">
-        <table class="detail_info">
-          <tr>
-            <th>カテゴリ</th>
-            <td>ブラウス</td>
-          </tr>
-          <tr>
-            <th>商品の状態</th>
-            <td>目立った傷や汚れ無し</td>
-          </tr>
-          <tr>
-            <th>発送までの日数</th>
-            <td>2～3日</td>
-          </tr>
-          <tr>
-            <th>クリーニングオプション</th>
-            <td>クリーニング可</td>
-          </tr>
-          <tr>
-            <th>集荷オプション</th>
-            <td>集荷を申し込む</td>
-          </tr>
-        </table>
+        <div class="detail_info">
+          <ul>
+            <li>カテゴリ</li>
+            <li><?php echo "ブラウス"; ?></li>
+          </ul>
+          <ul>
+            <li>商品の状態</li>
+            <li><?php echo "目立った傷や汚れ無し"; ?></li>
+          </ul>
+          <ul>
+            <li>発送までの日数</li>
+            <li><?php echo "2～3日"; ?></li>
+          </ul>
+          <ul>
+            <li>クリーニングオプション</li>
+            <li>クリーニング<?php echo "可"; ?></li>
+          </ul>
+          <ul>
+            <li>集荷オプション</li>
+            <li><?php echo "集荷を申し込む";?></li>
+          </ul>
+        </div>
       </section>
 
     </article>
 
     <form action="" method="post">
-      <button type="submit" name="delete" class="delete_btn btn">削除する</button>
-      <button type="submit" name="edit" class="edit_btn btn">商品情報を編集する</button>
+      <button type="submit" name="delete" class="delete_btn">削除する</button>
+      <button type="submit" name="edit" class="edit_btn">商品情報を編集する</button>
     </form>
 
   </main>
 
   <!-- ここにフッター -->
+  <footer></footer>
 
 </body>
 
