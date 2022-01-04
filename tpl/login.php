880f5adfb18a04cf4c4a2295cab814338f34b27f
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-05 08:11:09 +0900

    [update]デザインを変更

--- tpl/login.php
+++ tpl/login.php
@@ -10,22 +10,40 @@
 </head>
 
 <body>
+  <header>
+    <p>ログイン</p>
+  </header>
   <main>
+    <p class="logo">
+      <img src="./img/icon/hukurima.png" alt="logo" width="200px">
+    </p>
     <article>
-      <form action="./login.php" method="post">
-        <ul>
-          <li>ログインID</li>
-          <li><input type="text" name="login_id"></li>
-        </ul>
-        <ul>
-          <li>パスワード</li>
-          <li><input type="text" name="password"></li>
-        </ul>
-
-        <button type="submit" name="login_btn" value="on">ログイン</button>
-      </form>
+      <section>
+        <form action="./login.php" method="post">
+          <ul>
+            <li>ログインID</li>
+            <li><input type="text" name="login_id" placeholder="ログインIDを入力"></li>
+          </ul>
+          <ul>
+            <li>パスワード</li>
+            <li><input type="text" name="password" placeholder="パスワードを入力"></li>
+          </ul>
+          <p class="login-btn">
+            <button type="submit" name="login_btn" value="on">ログイン</button>
+          </p>
+          <p class="forget-text">＞＞ IDまたはパスワードをお忘れの方</p>
+          <hr>
+          <p class="entry-text">アカウントをお持ちでない方</p>
+          <p class="entry-btn">
+            <button type="submit" name="entry_btn">会員登録</button>
+          </p>
+        </form>
+      </section>
     </article>
   </main>
+  <footer class="global-footer">
+    <?php require_once('./footer.php'); ?>
+  </footer>
 </body>
 
 </html>
