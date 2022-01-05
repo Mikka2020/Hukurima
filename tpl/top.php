2eafb2e0b38725ad0a19109e8e4d11acad182c36
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-05 18:02:46 +0900

    [update]ハートアイコンをimgタグからsvgタグへ変更

--- tpl/top.php
+++ tpl/top.php
@@ -45,11 +45,17 @@
                 src="./img/users/<?php echo $product['user_id']; ?>/products/<?php echo $product['listing_id'] ?>/<?php echo $product['product_name']; ?>_1.<?php echo $product['img_extension']; ?>"
                 alt="商品" width="180px" height="" />
             </a>
-            <form action="" method="post" class="favorite-btn">
-              <button type="submit">
-                <img src="./img/icon/heart-regular.svg" alt="いいねアイコン" width="24px" height="">
+            <form action="" method="post">
+              <button type="submit" class="favorite-btn">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heart"
+                  class="svg-inline--fa fa-heart fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512">
+                  <path
+                    fill="<?php echo $product['favorite_listing_id'] == $product['listing_id'] ? "#FF8198" :"#e1e1e1" ; ?>"
+                    d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z">
+                  </path>
+                </svg>
               </button>
-
             </form>
           </div>
 
