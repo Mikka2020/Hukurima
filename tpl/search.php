7b8422177142f590b9b00c1c936071fed1e94c71
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-07 10:50:39 +0900

    [update]タグを整理

--- tpl/search.php
+++ tpl/search.php
@@ -17,59 +17,65 @@
 </head>
 
 <body>
-  <div class="wrapper">
+  <header class="wrapper">
     <input class="search_text" type="text" name="message" placeholder="商品を検索しよう" value="" />
     <button class="grass" type="submit" name="btn" value="up">
       <i class="fas fa-search"></i>
     </button>
-  </div>
-  <div class="keyword">
-    <h2>急上昇キーワード</h2>
-  </div>
-  <div class="keywords">
-    <button type="submit" name="btn" value="up">アウター</button>
-    <button type="submit" name="btn" value="up">ノースフェイス</button><br />
-    <button type="submit" name="btn" value="up">パーカー</button>
-    <button type="submit" name="btn" value="up">DIESEL</button>
-  </div>
+  </header>
 
-  <br />
-  <div class="search_values">
-    <table>
-      <th>
-        <button class="pink" type="submit" name="btn" value="up">
-          <i class="fas fa-tshirt"></i>
-        </button>
-        <h6>ブランドから検索</h6>
-      </th>
-      <th>
-        <button class="pink" type="submit" name="btn" value="up">
-          <i class="fa fa-book" aria-hidden="true"></i>
-        </button>
-        <h6>カタログから検索</h6>
-      </th>
-      <th>
-        <button class="pink" type="submit" name="btn" value="up">
-          <i class="fa fa-bars" aria-hidden="true"></i>
-        </button>
-        <h6>カテゴリから検索</h6>
-      </th>
-    </table>
-  </div>
-  <div class="save_search">
-    <h5>保存した検索結果一覧</h5>
-    <table>
-      <div class="search01">
-        <button>DIESEL　　　　　　　　　　　　　　　　　　...</button>
+  <main>
+    <section>
+      <div class="keyword">
+        <h2>急上昇キーワード</h2>
       </div>
-      <div class="search02">
-        <button>DIESEL、スウェット　　　　　　　　　　　　...</button>
+      <div class="keywords">
+        <button type="submit" name="btn" value="up">アウター</button>
+        <button type="submit" name="btn" value="up">ノースフェイス</button><br />
+        <button type="submit" name="btn" value="up">パーカー</button>
+        <button type="submit" name="btn" value="up">DIESEL</button>
       </div>
-      <div class="search03">
-        <button>DIESEL、パーカー　　　　　　　　　　　　　...</button>
-      </div>
-    </table>
-  </div>
+    </section>
+
+    <section class="search_values">
+      <table>
+        <th>
+          <button class="pink" type="submit" name="btn" value="up">
+            <i class="fas fa-tshirt"></i>
+          </button>
+          <h6>ブランドから検索</h6>
+        </th>
+        <th>
+          <button class="pink" type="submit" name="btn" value="up">
+            <i class="fa fa-book" aria-hidden="true"></i>
+          </button>
+          <h6>カタログから検索</h6>
+        </th>
+        <th>
+          <button class="pink" type="submit" name="btn" value="up">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </button>
+          <h6>カテゴリから検索</h6>
+        </th>
+      </table>
+    </section>
+
+    <section class="save_search">
+      <h5>保存した検索結果一覧</h5>
+      <table>
+        <div class="search01">
+          <button>DIESEL　　　　　　　　　　　　　　　　　　...</button>
+        </div>
+        <div class="search02">
+          <button>DIESEL、スウェット　　　　　　　　　　　　...</button>
+        </div>
+        <div class="search03">
+          <button>DIESEL、パーカー　　　　　　　　　　　　　...</button>
+        </div>
+      </table>
+    </section>
+  </main>
+
   <footer class="global-footer">
     <?php require_once('./footer.php'); ?>
   </footer>
