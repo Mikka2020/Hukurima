025bc4ebc7b68fb9f435652d67d8ec25383f1e39
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-11 18:59:05 +0900

    [add]新規作成

--- tpl/post.php
+++ tpl/post.php
@@ -5,11 +5,33 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/post.css">
   <title>Document</title>
 </head>
 
 <body>
-
+  <header>
+    <!-- <img src="./img/icon/hukurima.png" alt="" width="60px" height=""> -->
+    <p class="center">投稿</p>
+  </header>
+  <main>
+    <article>
+      <section>
+        <p class="textarea-box"><textarea name="" id="" cols="30" rows="10" maxlength="140"></textarea></p>
+        <div>
+          <button>
+            <img src="./img/icon/出品アイコン.svg" alt="" width="24px" height="">
+          </button>
+          <button>
+            <img src="./img/icon/紙袋アイコン.svg" alt="" width="24px" height="">
+          </button>
+        </div>
+      </section>
+    </article>
+  </main>
+  <footer class="global-footer">
+    <?php require_once('./sns_footer.php'); ?>
+  </footer>
 </body>
 
 </html>
