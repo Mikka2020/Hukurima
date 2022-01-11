7fb7fcb1680725a6f855f9407c6ec1574404405d
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-11 18:59:42 +0900

    [update]footerをパーツ化

--- tpl/timeline.php
+++ tpl/timeline.php
@@ -14,7 +14,7 @@
     <img src="./img/icon/hukurima.png" alt="" width="" height="30px">
   </header>
   <main>
-    <?php foreach($products_arr as $product): ?>
+    <?php foreach ($products_arr as $product) : ?>
     <section>
       <div class="post-div">
         <a href="">
@@ -52,40 +52,7 @@
     <?php endforeach; ?>
   </main>
   <footer class="global-footer">
-    <nav>
-      <ul>
-        <li>
-          <a href="">
-            <img src="./img/icon/ホームアイコン.svg" alt="ホームボタン"><br>
-            ホーム
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="./img/icon/さがすアイコン.svg" alt="探すボタン"><br>
-            さがす
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="./img/icon/DMアイコン.svg" alt="メッセージボタン"><br>
-            メッセージ
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="./img/icon/通知アイコン.svg" alt="通知ボタン"><br>
-            通知
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="./img/icon/マイページアイコン.svg" alt="マイページボタン"><br>
-            マイページ
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <?php require_once('./sns_footer.php'); ?>
   </footer>
 </body>
 
