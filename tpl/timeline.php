b3ebe30c6d1ed0e3e24541bb3126dccec3ed54c2
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-11 18:22:30 +0900

    [update]いいねボタンをsvgへ変更

--- tpl/timeline.php
+++ tpl/timeline.php
@@ -34,7 +34,15 @@
       </p>
 
       <p>
-        <img src="./img/icon/heart-regular.svg" alt="いいねボタン" width="24px" height="24px">5
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heart"
+          class="svg-inline--fa fa-heart fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+          width="24px" height="">
+          <path fill="<?php echo "#ccc"; ?>"
+            d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z">
+          </path>
+        </svg>
+
+        5
         <img src="./img/icon/返信アイコン.svg" alt="返信ボタン" width="24px" height="24px">1
       </p>
       <p>
