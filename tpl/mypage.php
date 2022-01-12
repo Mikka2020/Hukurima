5da4bcf22be5a7969348251be3b6db07a6779ebe
komaki216 <komaki.0025@gmail.com>
2022-01-12 20:16:27 +0900

    [update]HTMLを編集

--- tpl/mypage.php
+++ tpl/mypage.php
@@ -9,6 +9,59 @@
 </head>
 
 <body>
+  <h1>フクリマ</h1>
+
+  <header>
+    <h2>マイページ</h2>
+  </header>
+
+  <main>
+    <section>
+      <h3>会員設定</h3>
+      <ul>
+        <li><a href="./profile.php">プロフィール</a></li>
+        <li><a href="#">会員情報設定</a></li>
+        <li><a href="#">配送先管理</a></li>
+        <li><a href="#">売上金管理</a></li>
+        <li><a href="#">本人確認</a></li>
+      </ul>
+    </section>
+
+    <section>
+      <h3>ご利用情報</h3>
+      <ul>
+        <li><a href="#">下書き一覧</a></li>
+        <li><a href="#">いいねした商品</a></li>
+        <li><a href="#">閲覧履歴</a></li>
+        <li><a href="./listed_history_list.php">出品履歴</a></li>
+        <li><a href="#">購入履歴</a></li>
+        <li><a href="./trading_history_list.php">取引履歴</a></li>
+      </ul>
+    </section>
+
+    <section>
+      <h3>診断</h3>
+      <li><a href="#">骨格診断</a></li>
+      <li><a href="#">パーソナルカラー診断</a></li>
+    </section>
+
+    <section>
+      <h3>その他</h3>
+      <li><a href="#">通知設定</a></li>
+      <li><a href="#">ヘルプ</a></li>
+      <li><a href="#">お問い合わせ</a></li>
+    </section>
+
+    <section>
+      <ul>
+        <li><a href="./login.php">ログアウト</a></li>
+      </ul>
+    </section>
+  </main>
+
+  <footer class="global-footer">
+    <?php require_once('./footer.php'); ?>
+  </footer>
 
 </body>
 
