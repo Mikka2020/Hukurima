a023da40a36d2f7d91c238a2797ac4fb02809e0f
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-12 15:37:03 +0900

    [update]functionをfuncへ移し替え

--- timeline.php
+++ timeline.php
@@ -3,35 +3,6 @@
 require_once('./config.php');
 require_once('./func.php');
 
-// DBから全てのrecordを取り出す
-function get_db_records($sql)
-{
-  $link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
-  mysqli_set_charset($link, 'utf8');
-  $result = mysqli_query($link, $sql);
-  while ($row = mysqli_fetch_assoc($result)) {
-    $records[] = $row;
-  }
-  mysqli_close($link);
-  if (!isset($records)) {
-    return [];
-  }
-  return $records;
-}
-// DBからrecordを一件取得する、一致するレコードがなければ空配列を返す
-function get_db_record($sql)
-{
-  $link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
-  mysqli_set_charset($link, 'utf8');
-  $result = mysqli_query($link, $sql);
-  if (!$result) {
-    return [];
-  }
-  $record = mysqli_fetch_assoc($result);
-  mysqli_close($link);
-  return $record;
-}
-
 
 // postsとprofileを結合
 $sql = "SELECT * FROM posts ";
