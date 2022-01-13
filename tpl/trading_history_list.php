d3d4a3adcaac9734c685528c73421cd02ed8423e
komaki216 <komaki.0025@gmail.com>
2022-01-13 15:59:41 +0900

    [create]新規作成

---
+++ tpl/trading_history_list.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/trading_history_list.css">
+  <title>フクリマ 取引一覧</title>
+</head>
+<body>
+  <header>
+    <p class="back-icon">
+      <a href="#" onClick="history.back(); return false;">
+        <img src="./img/icon/arrow_back_ios_new_black_24dp.svg" alt="" width="24px">
+      </a>
+    </p>
+    <p>取引一覧</p>
+  </header>
+
+  
+  
+</body>
+</html>
---
+++ trading_history_list.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once './tpl/trading_history_list.php';
