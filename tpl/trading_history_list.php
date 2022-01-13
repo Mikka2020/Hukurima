ba8b20129db24968828fcc4955831ce88ea90620
komaki216 <komaki.0025@gmail.com>
2022-01-13 17:10:48 +0900

    [add]取引一覧画面新規作成

--- tpl/trading_history_list.php
+++ tpl/trading_history_list.php
@@ -8,16 +8,39 @@
   <title>フクリマ 取引一覧</title>
 </head>
 <body>
+  <h1>フクリマ</h1>
+
   <header>
     <p class="back-icon">
       <a href="#" onClick="history.back(); return false;">
         <img src="./img/icon/arrow_back_ios_new_black_24dp.svg" alt="" width="24px">
       </a>
     </p>
-    <p>取引一覧</p>
+    <h2>取引一覧</h2>
   </header>
 
-  
+  <nav class="sort-nav">
+    <ul>
+      <li>並び替え</li>
+      <li>絞り込み</li>
+    </ul>
+  </nav>
+
+  <main>
+    <section class="product">
+      <p class="product-img"><img src="./img/users/1/products/1/リリーコート_1.jpg" alt="商品画像" width="70px" height="70px"></p>
+      <p class="sold-mark"><img src="./img/icon/SOLDマーク.png" alt="購入済みマーク"></p>
+      <ul class="product-info">
+        <li class="buy-request-area"><button class="buy-request-btn"></button></li>
+        <li class="product-name">リリーコート</li>
+        <li class="product-price">¥3,600</li>
+      </ul>
+      <div><a href="#"><img src="./img/icon/進むアイコン.png" alt="進むボタン"></a></div>
+    </section>
+  </main>
+
+
+
   
 </body>
 </html>
