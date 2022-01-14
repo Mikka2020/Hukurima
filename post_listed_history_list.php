9d1f5a92c0251a48befc87a72889d3d6e2c6bb6b
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-14 19:04:42 +0900

    [add]出品履歴を一覧表示

---
+++ post_listed_history_list.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once('./config.php');
+require_once('./func.php');
+
+$id = $_COOKIE['user_id'];
+
+$sql = "SELECT * FROM ";
+$sql .= " listing ";
+$sql .= " WHERE ";
+$sql .= " user_id =  ";
+$sql .= $id;
+
+$products_arr = get_db_records($sql);
+// var_dump($products_arr);
+
+if (isset($_POST['select_submit'])) {
+  $_SESSION['listing_id'] = $_POST['listing_product'];
+
+  header('location:./post.php');
+  exit;
+}
+
+
+require_once('./tpl/post_listed_history_list.php');
