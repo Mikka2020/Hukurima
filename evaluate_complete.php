b0d496bd93817f5cc7b84447e27d748396520949
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-17 03:33:09 +0900

    [add]新規作成

--- evaluate_complete.php
+++ evaluate_complete.php
@@ -3,5 +3,12 @@
 require_once('./config.php');
 require_once('./func.php');
 
+// $listing_id = $_SESSION['bought_product_info'];
+$listing_id = 20;
+// session_destroy();
+
+$sql = "SELECT * FROM listing WHERE listing_id = " . $listing_id;
+$product = get_db_record($sql);
+
 
 require_once('./tpl/evaluate_complete.php');
