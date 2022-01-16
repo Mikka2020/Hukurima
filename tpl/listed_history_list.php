0b28a3429fc7c0c4b6edde0b78dac91a3e937ea4
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-16 19:42:42 +0900

    [add]listed_historyへ遷移できるように変更

--- tpl/listed_history_list.php
+++ tpl/listed_history_list.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/listed_history_list.css" />
-    <title>フクリマ 出品履歴一覧</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/listed_history_list.css" />
+  <title>フクリマ 出品履歴一覧</title>
 </head>
+
 <body>
 
   <header>
@@ -29,20 +31,24 @@
     <?php foreach($listing_arr as $value):?>
     <section>
       <p class="product-img">
-        <img src="./img/users/<?php echo $value['user_id'];?>/products/<?php echo $value['listing_id'];?>/<?php echo $value['product_name'] ;?>_1.<?php echo $value['img_extension'];?>" alt="商品画像" width="70px" height="70px">
+        <img
+          src="./img/users/<?php echo $value['user_id'];?>/products/<?php echo $value['listing_id'];?>/<?php echo $value['product_name'] ;?>_1.<?php echo $value['img_extension'];?>"
+          alt="商品画像" width="70px" height="70px">
         <img src="./img/icon/SOLDマーク.png" alt="購入済みマーク" class="sold-mark <?php echo $value['dealing_state'];?>">
       </p>
       <ul>
         <li class="requests<?php echo $value['requests_sum'];?>">
           <form action="receive_request.php" method="post">
-            <button type="submit" class="requests-btn" name="requests-btn" value="<?php echo $value['listing_id'];?>">購入申請<?php echo $value['requests_sum'];?>件</button>
+            <button type="submit" class="requests-btn" name="requests-btn"
+              value="<?php echo $value['listing_id'];?>">購入申請<?php echo $value['requests_sum'];?>件</button>
           </form>
         </li>
         <li class="product-name"><?php echo $value['product_name'];?></li>
         <li class="product-price">¥<?php echo $value['price'];?></li>
       </ul>
-      <form action="listed_history.php" method="post">
-        <button type="submit" name="listed_id" value="<?php echo $value['listed_id'];?>"><img src="./img/icon/進むアイコン.png" alt="進むボタン"></button>
+      <form action="listed_history.php?id=<?php echo $value['listing_id']; ?>" method="post">
+        <button type="submit" name="listed_id" value="<?php echo $value['listed_id'];?>"><img
+            src="./img/icon/進むアイコン.png" alt="進むボタン"></button>
       </form>
     </section>
     <?php endforeach;?>
@@ -52,6 +58,7 @@
     <?php require_once('./footer.php'); ?>
   </footer>
 
-    
+
 </body>
+
 </html>
