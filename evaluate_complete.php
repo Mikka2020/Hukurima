a0881afac375fb9a79fbfc357195d2c8e4716e9c
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-17 03:34:47 +0900

    [fix]

--- evaluate_complete.php
+++ evaluate_complete.php
@@ -3,12 +3,9 @@
 require_once('./config.php');
 require_once('./func.php');
 
-// $listing_id = $_SESSION['bought_product_info'];
-$listing_id = 20;
-// session_destroy();
+session_destroy();
 
-$sql = "SELECT * FROM listing WHERE listing_id = " . $listing_id;
-$product = get_db_record($sql);
+
 
 
 require_once('./tpl/evaluate_complete.php');
