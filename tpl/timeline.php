80b9581301947a8fd1051f9d871b1349e43aea55
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-17 05:45:05 +0900

    [update]CSS変更

--- tpl/timeline.php
+++ tpl/timeline.php
@@ -28,7 +28,7 @@
           <?php echo $product['nickname']; ?>
           <img class="menu" src="./img/icon/三点リーダーアイコン.svg" alt="メニューボタン" width="18px" height="18px">
         </div>
-        <p><?php echo $product['post_sentence']; ?></p>
+        <p class="post-sentence"><?php echo $product['post_sentence']; ?></p>
 
         <p class="listing-url">
           <a href="product_detail.php?id=<?php echo $product['listing_id']; ?>">
@@ -41,16 +41,16 @@
             alt="投稿画像" width="320px" height="320px">
         </p>
 
-        <p>
+        <p class="action-btns">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heart"
             class="svg-inline--fa fa-heart fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-            width="24px" height="24px">
+            width="20px" height="20px">
             <path fill="<?php echo "#ccc"; ?>"
               d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z">
             </path>
           </svg>
           0
-          <img src="./img/icon/返信アイコン.svg" alt="返信ボタン" width="24px" height="24px">0
+          <img src="./img/icon/返信アイコン.svg" alt="返信ボタン" width="20px" height="20px">0
         </p>
         <p>
           <img src="./img/icon/時計アイコン.svg" alt="投稿時間"><?php echo mb_substr($product['post_at'], 0, -3); ?>
