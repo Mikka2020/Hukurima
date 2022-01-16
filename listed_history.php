a8a02e3ed6e91360c337da383e6c91dced8196a0
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-16 13:12:10 +0900

    [add]出品者以外がアクセスできないように処理を追加

--- listed_history.php
+++ listed_history.php
@@ -7,8 +7,14 @@ require_once('./func.php');
 // DBから商品レコードを１つ取得
 $link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
 $table = 'listing';
-$list = get_column($link, $table, 'listing_id', 1);
+$list = get_column($link, $table, 'listing_id', $_GET['id']);
 mysqli_close($link);
+
+// 出品者以外がアクセスしたとき
+if ($list['user_id'] != $_COOKIE['user_id']) {
+  header('location:./product_detail.php?id='.$_GET['id']);
+  exit;
+}
 
 // var_dump($list);
 
