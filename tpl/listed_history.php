4289892162b909c0f79e5ea50b7dca323f9acc20
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-16 14:08:21 +0900

    [update]CSS変更

--- tpl/listed_history.php
+++ tpl/listed_history.php
@@ -13,9 +13,11 @@
 <body>
   <h1>フクリマ</h1><!-- CSSで消す -->
 
-  <header></header>
+  <header>
+    <img src="./img/icon/hukurima.png" alt="" width="" height="30px">
+  </header>
   <main>
-    <h2>出品履歴詳細</h2>
+    <h2 class="none">出品履歴詳細</h2>
     <article>
       <img
         src="./img/users/<?php echo $list['user_id']; // 会員id ?>/products/<?php echo $list['listing_id']; // 商品id ?>/<?php echo $list['product_name']; // 商品名 ?>_1.jpg"
@@ -23,13 +25,15 @@
 
       <!-- 商品情報 -->
       <section>
-        <button class="request_btn">購入申請が1件届いています！</button>
-        <h3 class="product_name"><?php echo $list['product_name']; ?></h3>
+        <form action="" method="POST">
+          <button class="request-btn">購入申請が1件届いています！</button>
+        </form>
+        <h3 class="product-name"><?php echo $list['product_name']; ?></h3>
         <ul>
-          <li class="brand_name"><?php echo $list['brand']; ?></li>
-          <li class="product_price">¥<?php echo $list['price']; ?></li>
+          <li class="brand"><?php echo $list['brand']; ?></li>
+          <li class="price">¥<?php echo $list['price']; ?></li>
           <li>
-            <ul>
+            <ul class="btn-wrapper">
               <li>
                 <button class="product_btn">
                   <img src="./img/icon/いいねアイコン.png" alt="いいねアイコン" class="favorite_icon"><?php echo "3"; ?>
@@ -43,33 +47,27 @@
             </ul>
           </li>
         </ul>
+        <p>
+          <img src="./img/icon/clock-regular.svg" alt="時計アイコン" width="12px" height="">
+          <?php echo $list['listed_at']; ?>
+        </p>
       </section>
 
       <h4>商品説明</h4>
-      <section class="product_info_area">
+      <section class="product-info-area">
         <div>
-          <p class="product_text"><?php echo $list['product_explain']; // 商品詳細 ?>
+          <p class="product-text">
+            <?php echo $list['product_explain']; ?>
           </p>
-          <p>
-            <img src="./img/icon/clock-regular.svg" alt="時計アイコン" width="12px" height="">
-            <?php echo $list['listed_at']; // 出品日時 ?>
-          </p>
-        </div>
-        <div class="tag_area">
-          <ul class="tag_ul">
-            <?php // foreach($tags as $tag): ?>
-            <li class="tag"><?php echo "#ニット"; ?></li>
-            <?php // endforeach; ?>
-          </ul>
         </div>
       </section>
 
       <h4>商品詳細</h4>
-      <section class="product_info_area">
+      <section class="product-info-area">
         <div class="detail_info">
           <ul>
             <li>カテゴリ</li>
-            <li><?php echo $list['category']; ?></li>
+            <li><?php echo $list['product_category']; ?></li>
           </ul>
           <ul>
             <li>商品の状態</li>
@@ -92,15 +90,17 @@
 
     </article>
 
-    <form action="" method="post">
-      <button type="submit" name="delete" class="delete_btn">削除する</button>
-      <button type="submit" name="edit" class="edit_btn">商品情報を編集する</button>
+    <form action="" method="post" class="btn-form">
+      <button type="submit" name="delete" class="delete-btn">削除する</button>
+      <button type="submit" name="edit" class="edit-btn">商品情報を編集する</button>
     </form>
 
   </main>
 
   <!-- ここにフッター -->
-  <footer></footer>
+  <footer class="global-footer">
+    <?php require_once('./footer.php'); ?>
+  </footer>
 
 </body>
 
