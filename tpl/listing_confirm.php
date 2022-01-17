27fe1e7b54a26d2cd0997935a9182ac4045264bc
komaki216 <komaki.0025@gmail.com>
2022-01-18 05:49:48 +0900

    [update]出品確認画面CSS更新

--- tpl/listing_confirm.php
+++ tpl/listing_confirm.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./css/listing_confirm.css">
-  <title>出品確認</title>
+  <title>フクリマ 出品確認</title>
 </head>
 
 <body>
@@ -16,11 +16,6 @@
   </header>
   <main>
     <article>
-      <!-- <div class="center">
-        <img
-          src="./img/users/<?php //echo $user_id; ?>/pre/<?php //echo $listing['product_name']; ?>.<?php //echo $listing['img_extension']; ?>"
-          alt="メイン商品画像" width="360px" height="">
-      </div> -->
 
       <section class="img-section">
         <h3>商品画像</h3>
@@ -37,50 +32,38 @@
         </ul>
       </section>
 
+      <section class="product-info">
+        <h4>商品説明</h4>
+        <p><?php echo $listing['product_explain']; ?></p>
+      </section>
+
       <section class="product-detail">
-        <!-- <h3 class="product-name"><?php //echo $listing['product_name']; ?></h3>
-        <p class="brand"><?php //echo $listing['brand']; ?></p>
-        <p class="price red">¥<?php //echo $listing['price']; ?></p> -->
+        <h4>商品詳細</h4>
         <ul>
-          <li>
-            カテゴリ
-          </li>
-          <li>
-            <?php echo $listing['product_category']; ?>
-          </li>
+          <li>カテゴリ</li>
+          <li><?php echo $listing['product_category']; ?></li>
         </ul>
         <ul>
-          <li>
-            商品の状態
-          </li>
-          <li>
-            <?php echo $product_condition[$listing['product_condition']]; ?>
-          </li>
+          <li>商品の状態</li>
+          <li><?php echo $product_condition[$listing['product_condition']]; ?></li>
         </ul>
         <ul>
-          <li>発送までの日時</li>
+          <li>発送までの日数</li>
           <li><?php echo $days_to_ship[$listing['days_to_ship']]; ?></li>
         </ul>
         <ul>
-          <li>クリーニングオプション</li>
+          <li>クリーニング</li>
           <li><?php echo $cleaning[$listing['cleaning_flg']]; ?></li>
         </ul>
         <ul>
-          <li>集荷オプション</li>
-          <li><?php echo $picking[$listing['picking_flg']]; ?></li>
-        </ul>
-        <ul>
           <li>購入申請自動許可</li>
-          <li><?php echo $auto_approval[$listing['auto_approval_flg']]; ?></li>
+          <li>
+          <?php echo $auto_approval[$listing['auto_approval_flg']]; ?>
+          </li>
         </ul>
-        <p class="explain-title">商品説明文</p>
-        <p class="explain-text"><?php echo $listing['product_explain']; ?></p>
-
-
-
       </section>
-
     </article>
+
     <form action="#" method="post">
       <ul>
         <li class="listing-btn">
@@ -91,6 +74,7 @@
         </li>
       </ul>
     </form>
+
   </main>
   <footer class="global-footer">
     <?php require_once('./footer.php'); ?>
