b99720d4867a71c798a4803d7b0acf32484293f4
komaki216 <komaki.0025@gmail.com>
2022-01-18 07:06:53 +0900

    [update]購入完了画面追加(未完成)

--- bought_complete.php
+++ bought_complete.php
@@ -4,8 +4,16 @@ require_once('./config.php');
 require_once('./func.php');
 
 // 出品IDを入手
+$listing_id = '';
 $listing_id = $_SESSION['bought_listing_id'];
-unset($_SESSION['bought_listing_id']);
+// echo $listing_id;
+
+// $value = $listing_id;
 
 require_once('./tpl/bought_complete.php');
+
+// unset($_SESSION['bought_listing_id']);
+
+// echo $listing_id;
+
 ?>
