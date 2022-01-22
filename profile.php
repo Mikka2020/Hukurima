eb6663ea9371f7fb8e8417c3e8fecbd9a08dfbd7
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-23 06:43:02 +0900

    [add]DBから取得した情報を表示

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@
  * 変更日：2021/12/19
  * 追加内容：全体的な処理（正常処理のみ）
  * ------------------------------------------------------------------------------------------------------------------------
-*/
+ */
 
 
 // 参照ファイル呼び出し
@@ -16,35 +16,56 @@ session_start();
 require_once './config.php';
 require_once './func.php';
 
-// 動作確認用固定値
-setcookie('user_id',1,time()+60*60*24);
-
 // 初期値
-$list_table = 'listing';
-$member_table = 'member';
-$profile_table = 'profile';
 $user_id = $_COOKIE['user_id'];
 $column = 'user_id';
 
-// 「商品詳細画面」で見てた商品の商品IDを取得
-$link = mysqli_connect(HOST , USER_ID, PASSWORD , DB_NAME);
-mysqli_set_charset($link , 'utf8');
-$line = get_column($link,$profile_table,$column,$user_id);
-mysqli_close($link);
+// profileを取得
+$sql = "SELECT * FROM profile WHERE user_id = " . $user_id;
+$profile = get_db_record($sql);
 
-// 購入ボタンが押された時の処理
-if(isset($_POST['buy_btn'])){
-    $_SESSION['buy_product_info'] = $line;
-    header ('location:./.php');
-    exit;
+// 評価を取得
+{
+  // 出品時の評価
+  $sql = " SELECT ";
+  $sql .= " SUM(to_listing_evaluation) AS listing_eval, COUNT(*) AS listing_cnt ";
+  $sql .= " FROM ";
+  $sql .= " evaluations ";
+  $sql .= " WHERE ";
+  $sql .= " listing_user_id = " . $user_id;
+  $sql .= " AND to_listing_evaluation IS NOT NULL ";
+  $sql .= " GROUP BY listing_user_id ";
+  $listing_evaluation = get_db_record($sql);
+} {
+  // 購入時の評価
+  $sql = " SELECT ";
+  $sql .= " SUM(to_buyer_evaluation) AS buyer_eval, COUNT(*) AS buyer_cnt ";
+  $sql .= " FROM ";
+  $sql .= " evaluations ";
+  $sql .= " WHERE ";
+  $sql .= " buyer_user_id = " . $user_id;
+  $sql .= " AND to_buyer_evaluation IS NOT NULL ";
+  $sql .= " GROUP BY buyer_user_id ";
+  $buyer_evaluation = get_db_record($sql);
 }
+// 購入時と販売時を合わせた評価
+$evaluation['eval_val'] = ($listing_evaluation['listing_eval'] + $buyer_evaluation['buyer_eval']) / ($listing_evaluation['listing_cnt'] + $buyer_evaluation['buyer_cnt']);
+// 評価された回数
+$evaluation['eval_cnt'] = ($listing_evaluation['listing_cnt'] + $buyer_evaluation['buyer_cnt']);
+// 過去の出品履歴を取得
+$sql = " SELECT * ,listing.listing_id AS id ,dealings.listing_id AS dealing_listing_id ";
+$sql .= " FROM ";
+$sql .= " listing ";
+$sql .= " LEFT JOIN dealings ON dealings.listing_id = listing.listing_id ";
+$sql .= " WHERE listing.user_id = " . $user_id;
+$sql .= " ORDER BY listing.listed_at DESC ";
+$products_arr = get_db_records($sql);
+$listing_cnt = count($products_arr);
 
-// 戻るボタンが押された時の処理(name="back")
-// if(isset($_POST['back'])){
-//     header ('location:./');
-//     exit;
-// }
+// 投稿した数を取得
+$sql = " SELECT *, COUNT(*) AS post_cnt FROM posts WHERE member_id = " . $user_id;
+$post_cnt = get_db_record($sql)['post_cnt'];
 
-// var_dump($line);
+
 
 require_once './tpl/profile.php';
