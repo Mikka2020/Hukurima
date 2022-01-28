d194cf6807272e88cad2fc88e71581c5b64cf960
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-01-28 21:53:29 +0900

    [add]戻るボタン追加

--- tpl/profile.php
+++ tpl/profile.php
@@ -13,6 +13,11 @@
   <h1>フクリマ</h1>
 
   <header class="global-header">
+    <p class="back-icon">
+      <a href="#" onClick="history.back(); return false;">
+        <img src="./img/icon/arrow_back_ios_new_black_24dp.svg" alt="" width="24px">
+      </a>
+    </p>
     <h2 class="none">マイページ</h2>
     <img class="header-logo" src="./img/icon/hukurima.png" alt="" width="" height="30px">
   </header>
@@ -20,10 +25,9 @@
   <main>
     <article>
       <section class="user-info">
-        <img
-          class="profile-img"
+        <img class="profile-img"
           src="./img/users/<?php echo $profile['user_id']; ?>/profile/<?php echo $profile['nickname'] ?>.<?php echo $profile['prof_img_extension']; ?>"
-          alt="プロフィール画像" width="50px" height="50px" class="profile-img"/>
+          alt="プロフィール画像" width="50px" height="50px" class="profile-img" />
         <div>
           <h3><?php echo $profile['nickname']; ?></h3>
           <a href="">
@@ -31,7 +35,8 @@
               <?php echo mb_substr($evaluation['eval_val'], 0, 4); ?>
             </p>
           </a>
-          <p class="user-confirm"><img src="./img/icon/本人確認.png" alt="本人確認バッジ" width="14px" height="16px" /><a href="">本人確認する</a></p>
+          <p class="user-confirm"><img src="./img/icon/本人確認.png" alt="本人確認バッジ" width="14px" height="16px" /><a
+              href="">本人確認する</a></p>
         </div>
 
         <ul class="nav_prof">
@@ -73,12 +78,11 @@
       <section id="products">
         <?php  foreach($products_arr as $product): ?>
         <div>
-          <img
-            class="product-img"
+          <img class="product-img"
             src="./img/users/<?php echo $product['user_id']; ?>/products/<?php echo $product['id']; ?>/<?php echo $product['product_name']; ?>_1.<?php echo $product['img_extension']; ?>"
             alt="出品した商品" width="100px" height="100px">
-          <img id="sold" class="<?php echo $product['listing_id'] == NULL ? "none" : ""; ?>" src="./img/icon/SOLDマーク(出品履歴一覧).svg"
-            alt="">
+          <img id="sold" class="<?php echo $product['listing_id'] == NULL ? "none" : ""; ?>"
+            src="./img/icon/SOLDマーク(出品履歴一覧).svg" alt="">
         </div>
         <?php  endforeach; ?>
       </section>
